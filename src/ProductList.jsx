0a3b279d616d15d20ab9f2b43bb3409a627bf6c5
sele01 <sele.edu21@gmail.com>
2026-07-26 10:22:25 -0400

    Refactor : intergrate redux functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import addItem from './CartSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+
+    const dispatch = useDispatch(); 
+    const cartItems = useSelector(state => state.cart.items || []); 
+
+    const calculateTotalQuantity = () => {
+        return cartItems.reduce((total, item) => total + item.quantity, 0);
+    };
 
     const plantsArray = [
         {
